19e2e8dfa5a6b42c756482010ab6f265178f31da
inkizzang <rookieceo@gmail.com>
2020-02-18 22:46:07 +0900

    add document.write code for google adsense

--- assets/js/custom-js-adsense.js
+++ assets/js/custom-js-adsense.js
@@ -1,1 +1,2 @@
-<script data-ad-client="ca-pub-6904431799087937" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+document.write('<script data-ad-client="ca-pub-6904431799087937" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+
